887612844991579230466b5238355ec74a7b92be
mahadi1951 <mahadihassan1951@gmail.com>
2025-09-19 22:27:27 +0600

    first commit

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,19 @@
+import React, { use } from 'react';
+
+
+const Countries = ({countriesPromise}) => {
+    const countriesData = use(countriesPromise);
+    const countries = countriesData.countries;
+   
+    return(
+        <div>
+            <h1>In the countries:{countries.length}</h1>
+            {
+                
+            }
+        </div>
+    )
+}
+
+
+export default Countries;
